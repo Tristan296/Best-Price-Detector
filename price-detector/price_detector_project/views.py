e2461b5a3b100f7a11c9a27fb4c09097d067e4ff
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-07-02 10:09:56 +1000

    removed bing lee as it blocks webscraping.

--- price-detector/price_detector_project/views.py
+++ price-detector/price_detector_project/views.py
@@ -5,6 +5,7 @@ import itertools
 import random
 import time
 
+
 class LinkExtractor:
     def __init__(self, product, website_name):
         self.product = product
@@ -18,13 +19,14 @@ class LinkExtractor:
         if website_name == "rebelsport":
             # Replace whitespaces in the product name to match the URL format.
             product_formatted = self.product.replace(" ", "/")
-            # url = f"https://www.{website_name}.com.au/{product_formatted}?search_term={product_end_formatted}"
-            sort_option = input(""" Recommended for you is default sort
+            sort_option = input(
+                """ Recommended for you is default sort
             1. New In
             2. Top Rated
             3. Price: Low to High
             4. Price: High to Low
-            """)
+            """
+            )
             if sort_option == "1":
                 url = f"https://www.{website_name}.com.au/search?q={product_end_formatted}&srule=most-recent&start=0&sz=60"
             elif sort_option == "2":
@@ -35,16 +37,20 @@ class LinkExtractor:
                 url = f"https://www.{website_name}.com.au/search?q={product_end_formatted}&srule=price-descending&start=0&sz=60"
             elif sort_option == "":
                 url = f"https://www.{website_name}.com.au/{product_end_formatted}&srule=recommended-for-you&start=0&sz=60"
-                
+
             print(url)
         elif website_name == "harveynorman":
-            sort_option = int(input(""" Relevance is default sort
+            sort_option = int(
+                input(
+                    """ Relevance is default sort
             1. Sort by Price: Low to High
             2. Sort by Price High to Low
             3. Name: A to Z
             4. Name: Z to A
             5. Highest Rated
-            """))
+            """
+                )
+            )
             if sort_option == 1:
                 url = f"https://www.{website_name}.com.au/catalogsearch/result/index/?dir=asc&order=price&q={product}#toolbar-top"
             elif sort_option == 2:
@@ -58,25 +64,29 @@ class LinkExtractor:
             elif sort_option == None:
                 url = f"https://www.{website_name}.com.au/catalogsearch/result/?q={product}"
             else:
-                print("Please ensure you pick the above options from 1-7. e.g. input: '1'")
-        elif website_name == "binglee":
-            url = f"https://www.{website_name}.com.au/search?q={product}"
+                print(
+                    "Please ensure you pick the above options from 1-7. e.g. input: '1'"
+                )
         elif website_name == "thegoodguys":
             url = f"https://www.{website_name}.com.au/SearchDisplay?categoryId=&storeId=900&catalogId=30000&langId=-1&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&orderBy=0&pageSize=30&searchTerm={product_formatted}"
         elif website_name == "kogan":
             url = f"https://www.{website_name}.com/au/shop/?q={product_formatted}"
         elif website_name == "officeworks":
-            url = f"https://www.{website_name}.com.au/shop/{website_name}/search?q={product}&sortBy=bestmatch"
+            url = f"https://www.officeworks.com.au/shop/officeworks/search?q={product_end_formatted}&view=grid&page=1&sortBy=bestmatch"
+            # url = f"https://www.{website_name}.com.au/shop/{website_name}/search?q={product}&sortBy=bestmatch"
         else:
-            print("Please ensure you have spelt the website correctly and chosen from the options.")
+            print(
+                "Please ensure you have spelt the website correctly and chosen from the options."
+            )
         print(url)
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         }
         response = self.get_with_retries(url, headers)
         if response is not None:
             soup = BeautifulSoup(response.content, "lxml")
             return soup
+        # Response from url is None - meaning website content isn't working.
         else:
             print("The site's content could not be reached.")
             exit()
@@ -97,8 +107,8 @@ class LinkExtractor:
         soup = self.read_page()
         urls = []
         print("Links that contain " + str(self.product))
-        for link in soup.find_all('a'):
-            href = link.get('href')
+        for link in soup.find_all("a"):
+            href = link.get("href")
             if href is not None and str(self.product) in href:
                 urls.append(href)
         return urls
@@ -110,27 +120,29 @@ class ProductDetailsExtractor:
 
     def get_product_details_harveynorman(self):
         products = []
-        count = 0       
+        count = 0
         product_items = self.soup.select(".product-item")
         for item in product_items:
             name = item.select_one("a.name").text.strip()
             price = item.select_one("span.price").text.strip()
             product = {"Name": name, "Price": price, "Count": count}
             products.append(product)
-            count+=1
+            count += 1
         return products
 
-    
+    # Not working
     def get_product_details_officeworks(self):
         products = []
-        product_items = self.soup.select(".sc-eNQAEJ Tile-iqbpf7-0 inMdYv")
+        product_items = self.soup.select(
+            ".TileWrapper__TileWrapperStyled-sc-1ck0iwt-0.gFALXK"
+        )
         for item in product_items:
-            name = item.select_one("h5.DefaultProductTile__ProductName-dfe2sm-1 dRgJNf").text.strip()
-            price = item.select_one(".ProductPrice__Wrapper-sc-1ye3dgu-0 cXoUph").text.strip()
-            product = {"Name": name, "Price": f"${price}"}
+            name = item.select_one(".default-product-title").text.strip()
+            price = item.select_one(".price-clone").text.strip()
+            product = {"Name": name, "Price": price}
             products.append(product)
         return products
-    
+
     def get_product_details_thegoodguys(self):
         products = []
         count = 1
@@ -143,7 +155,8 @@ class ProductDetailsExtractor:
             products.append(product)
             count += 1
         return products
-    
+
+    # Not working
     def get_product_details_kogan(self):
         products = []
         product_items = self.soup.select("._1umis")
@@ -154,16 +167,6 @@ class ProductDetailsExtractor:
             products.append(product)
         return products
     
-    def get_product_details_binglee(self):
-        products = []
-        product_items = self.soup.select(".product-list-item")
-        for item in product_items:
-            name = item.select_one("a.block").text.strip()
-            price = item.select_one("span.pb-1").text.strip()
-            product = {"Name": name, "Price": price}
-            products.append(product)
-        return products
-  
     def get_product_details_rebelsport(self):
         products = []
         count = 0
@@ -171,14 +174,14 @@ class ProductDetailsExtractor:
         for item in product_items:
             name = item.select_one(".name-link").text.strip()
             price = item.select_one("span.price-sales").text.strip()
-            price = re.search(r'\d+\.\d+', price).group()
+            price = re.search(r"\d+\.\d+", price).group()
             product = {"Name": name, "Price": f"${price}", "Count": count}
             products.append(product)
-            count+=1
+            count += 1
         return products
-    
 
-    # Retrieve user input
+
+# Retrieve user input
 product = input("Type a product: ")
 website_name = input("Type a website: ")
 # Create a new instance of the LinkExtractor class
@@ -186,14 +189,14 @@ link_extractor = LinkExtractor(product, website_name)
 # Retrieve the links and soup
 links = link_extractor.extract_links()
 soup = link_extractor.read_page()
-    
+
 
 if "rebel" in website_name:
     print("Scraping Rebel Sports...")
     details_extractor = ProductDetailsExtractor(soup)
     product_details = details_extractor.get_product_details_rebelsport()
     print(product_details)
-        
+
 elif "harvey" in website_name and "norman" in website_name:
     print("Scraping Harvey Norman...")
     details_extractor = ProductDetailsExtractor(soup)
@@ -206,15 +209,9 @@ elif "good" in website_name and "guys" in website_name:
     product_details = details_extractor.get_product_details_thegoodguys()
     print(product_details)
 
-elif "bing" in website_name and "lee" in website_name:
-    print("Scraping Bing Lee...")
-    details_extractor = ProductDetailsExtractor(soup)
-    product_details = details_extractor.get_product_details_binglee()
-    print(product_details)
-    
 elif "officeworks" in website_name:
     print("Scraping Officeworks...")
     details_extractor = ProductDetailsExtractor(soup)
-    print(soup.find_all(''))
+    print(soup.find_all(""))
     product_details = details_extractor.get_product_details_officeworks()
     print(product_details)
